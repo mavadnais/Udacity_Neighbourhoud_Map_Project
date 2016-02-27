b255d76d484dd8bcc2d5c3fd212192950b3e5eb4
Marc-Antoine Vadnais <ma_vadnais@hotmail.com>
2016-02-27 17:31:28 -0500

    Added first test marker.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 
+var markersArray = [
+    {
+        title: 'Marker Test 1',
+        globalPosition: {lat: -25.363, lng: 131.044},
+        marker: null
+    }  
+];
 
 $(function(){
 
--- js/map.js
+++ js/map.js
@@ -1,7 +1,17 @@
 function initMap() {
+    //var myLatLng = {lat: -25.363, lng: 131.044};
+    
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map')/*$('#map').get(0)*/, {
-        center: {lat: -34.397, lng: 150.644},
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: markersArray[0].globalPosition,
         zoom: 8
     });
+    
+    for (var i = 0;  i < markersArray.length; i++) {
+        markersArray[i].marker = new google.maps.Marker({
+            position: markersArray[i].globalPosition,
+            map: map,
+            title: markersArray[i].title
+        });
+    }
 }
