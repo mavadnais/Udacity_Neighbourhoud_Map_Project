0daf91ec77869f8035f0bca90614a9ea2c9a188d
Marc-Antoine Vadnais <ma_vadnais@hotmail.com>
2016-02-28 16:08:32 -0500

    Added 5 markers to map.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 
-var markersArray = [
+var g_markersArray = [
     {
-        title: 'Marker Test 1',
-        globalPosition: {lat: -25.363, lng: 131.044},
+        title: "Schwartz's Deli",
+        address: '3895 Boul St-Laurent, Montréal, QC H2W 1X9',
         marker: null
-    }  
+    }, 
+    {
+        title: 'Vol De Nuit',
+        address: '14 Rue Prince Arthur E, Montréal, QC H2X 1S3',
+        marker: null
+    }, 
+    {
+        title: 'Café Saint Laurent Frappé',
+        address: '3900 Boul St-Laurent, Montréal, QC H2W 1Y2',
+        marker: null
+    }, 
+    {
+        title: 'Bar Bifteck St-Laurent',
+        address: '3702 Boul St-Laurent, Montréal, QC H2X 2V4',
+        marker: null
+    }, 
+    {
+        title: 'La Belle Province',
+        address: '3608, boul Saint-Laurent, Montréal, QC H2X 2V4',
+        marker: null
+    }      
 ];
 
 $(function(){
--- js/map.js
+++ js/map.js
@@ -1,17 +1,39 @@
+
+var g_map;
+var g_geocoder;
+
 function initMap() {
-    //var myLatLng = {lat: -25.363, lng: 131.044};
-    
+ 
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: markersArray[0].globalPosition,
-        zoom: 8
+    g_map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15
     });
     
-    for (var i = 0;  i < markersArray.length; i++) {
-        markersArray[i].marker = new google.maps.Marker({
-            position: markersArray[i].globalPosition,
-            map: map,
-            title: markersArray[i].title
-        });
+    // Create geocoder for converting addresses to longitude/latitude
+    g_geocoder = new google.maps.Geocoder();
+    
+    // Add markers to the map
+    for (var i = 0;  i < g_markersArray.length; i++) {
+        addMarkerToMap(g_markersArray[i]);
     }
+}
+
+function addMarkerToMap(p_marker) {
+    g_geocoder.geocode( { 'address': p_marker.address}, function(p_results, p_status) {
+        if (p_status == google.maps.GeocoderStatus.OK) {
+            // Set the center to this latest marker
+            g_map.setCenter(p_results[0].geometry.location);
+            
+            // Create the new marker on the map
+            p_marker.marker = new google.maps.Marker({
+                position: p_results[0].geometry.location,
+                map: g_map,
+                title: p_marker.title
+            });
+        } 
+        else {
+            // TODO: better error handling in message box under title
+            alert('Geocode was not successful for the following reason: ' + p_status);
+        }
+    });
 }
