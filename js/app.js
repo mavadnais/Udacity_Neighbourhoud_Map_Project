90455311232d22d9f0d802691f5c9c2ae7fdd221
Marc-Antoine Vadnais <ma_vadnais@hotmail.com>
2016-03-16 21:46:18 -0400

    Progress.

--- js/app.js
+++ js/app.js
@@ -3,49 +3,97 @@ var g_markersArray = [
     {
         title: "Schwartz's Deli",
         address: '3895 Boul St-Laurent, Montréal, QC H2W 1X9',
-        marker: null
+        infoContent: 'Test',
+        mapMarker: null,
+        geoLocation: null,
+        infoWindow: null
     }, 
     {
         title: 'Vol De Nuit',
         address: '14 Rue Prince Arthur E, Montréal, QC H2X 1S3',
-        marker: null
+        infoContent: 'Test',
+        mapMarker: null,
+        geoLocation: null,
+        infoWindow: null
     }, 
     {
         title: 'Café Saint Laurent Frappé',
         address: '3900 Boul St-Laurent, Montréal, QC H2W 1Y2',
-        marker: null
+        infoContent: 'Test',
+        mapMarker: null,
+        geoLocation: null,
+        infoWindow: null
     }, 
     {
         title: 'Bar Bifteck St-Laurent',
         address: '3702 Boul St-Laurent, Montréal, QC H2X 2V4',
-        marker: null
+        infoContent: 'Test',
+        mapMarker: null,
+        geoLocation: null,
+        infoWindow: null
     }, 
     {
         title: 'La Belle Province',
         address: '3608, boul Saint-Laurent, Montréal, QC H2X 2V4',
-        marker: null
+        infoContent: 'Test',
+        mapMarker: null,
+        geoLocation: null,
+        infoWindow: null
     }      
 ];
 
+var g_testFailures = {
+    testGeocodeFailure: false  
+};
+
 $(function(){
+    // Set map container width and height relative to the window width and height
+    // Couldn't figure out how to do it with css percentages.
     var mapWidth = $(window).width() * 0.85;
     var mapHeight = $(window).height() * 0.9;
-    console.log("width: " + mapWidth + ", height: " + mapHeight);
-    $('#map_container').width(mapWidth + 'px');
-    $('#map_container').height(mapHeight + 'px');
+    $('#map').width(mapWidth + 'px');
+    $('#map').height(mapHeight + 'px');
     
+    // Populate the menu of markers 
     for (var i = 0; i < g_markersArray.length; i++) {
         $('#main_menu').append(
-            '<div class="menu_item pointer_cursor"> - ' + g_markersArray[i].title + '</div>'  
+            '<div class="menu_item pointer_cursor" onclick="openInfoWindowByIndex(' + i + ')"> - ' + g_markersArray[i].title + '</div>'  
         );
     }
 });
 
 function toggleMenuContainer() {
-    //$('#menu_container').toggle()
+    $('#menu_container').toggle()
 }
 
 function updateFilter() {
     console.log('in updateFilter: ' + $('#filter_text_field').val());
+}
+
+function displayMessage(p_message, p_messageType) {
+    var messageDisplay = $('#message_display');
+    messageDisplay.show()
+    messageDisplay.text(p_message);
+    
+    // Change text color depending on type of message
+    switch (p_messageType) {
+        case 'negative':
+            messageDisplay.addClass('negative_message');
+            break;
+        case 'positive':
+            messageDisplay.addClass('positive_message');
+            break;
+        default:
+            messageDisplay.addClass('neutral_message');
+    }
+    
+    // In 10 seconds hide the message
+    window.setTimeout(function() {
+        messageDisplay.hide()
+        messageDisplay.text('');
+        messageDisplay.removeClass('negative_message');
+        messageDisplay.removeClass('positive_message');
+        messageDisplay.removeClass('neutral_message');
+    }, 10000);
 }
 
--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ function initMap() {
  
     // Create a map object and specify the DOM element for display.
     g_map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15
+        zoom: 16
     });
     
     // Create geocoder for converting addresses to longitude/latitude
@@ -20,20 +20,50 @@ function initMap() {
 
 function addMarkerToMap(p_marker) {
     g_geocoder.geocode( { 'address': p_marker.address}, function(p_results, p_status) {
-        if (p_status == google.maps.GeocoderStatus.OK) {
+        if (p_status == google.maps.GeocoderStatus.OK && !g_testFailures.testGeocodeFailure) {
             // Set the center to this latest marker
-            g_map.setCenter(p_results[0].geometry.location);
+            p_marker.geoLocation = p_results[0].geometry.location;
+            g_map.setCenter(p_marker.geoLocation);
             
             // Create the new marker on the map
-            p_marker.marker = new google.maps.Marker({
-                position: p_results[0].geometry.location,
+            p_marker.mapMarker = new google.maps.Marker({
+                position: p_marker.geoLocation,
                 map: g_map,
                 title: p_marker.title
+            });
+            
+            // Create info window
+            p_marker.infoWindow = new google.maps.InfoWindow({
+                content: p_marker.infoContent
+            });
+            
+            // Add listener so info window opens when marker is clicked
+            p_marker.mapMarker.addListener('click', function() {
+                openInfoWindowByMarker(p_marker);
             });
         } 
         else {
             // TODO: better error handling in message box under title
-            alert('Geocode was not successful for the following reason: ' + p_status);
+            //alert('Geocode was not successful for the following reason: ' + p_status);
+            displayMessage('ERROR: Geocode was not successful for the following reason: ' + p_status, 'negative');
         }
     });
+}
+
+function openInfoWindowByIndex(p_markerIndex) {
+    var marker = g_markersArray[p_markerIndex];
+    openInfoWindowByMarker(marker);
+}
+
+function openInfoWindowByMarker(p_marker) {
+     // Close all other info windows
+    for (var i = 0;  i < g_markersArray.length; i++) {
+        if (g_markersArray[i] != p_marker) {
+            g_markersArray[i].infoWindow.close();    
+        }
+    }
+    
+    // Open info window and center on marker
+    p_marker.infoWindow.open(g_map, p_marker.mapMarker);
+    g_map.setCenter(p_marker.geoLocation);
 }
